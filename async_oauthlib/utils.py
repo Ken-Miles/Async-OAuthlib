99cc7cfffeb10936bb9ba9c282094b9b21c33da2
jmulligan <sungspoon804323@gmail.com>
2024-05-09 23:13:03 -0500

    changed password to client_secret

--- async_oauthlib/utils.py
+++ async_oauthlib/utils.py
@@ -3,5 +3,5 @@ import aiohttp
 
 class BasicAuth(aiohttp.BasicAuth):
 
-    def __new__(cls, login, password, **kwargs):
-        return super(BasicAuth, cls).__new__(cls, str(login), str(password), **kwargs)
+    def __new__(cls, login, client_secret, **kwargs):
+        return super(BasicAuth, cls).__new__(cls, str(login), str(client_secret), **kwargs)
